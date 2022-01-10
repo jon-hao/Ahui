381810380ae6e24f58d8e730fd33f0e108a2d9ec
Albatrosses <capitalism_4418@163.com>
2022-01-10 10:50:33 +0000

    Albatrosses: remove UI components

--- src/components/mobile/index.ts
+++
@@ -1,7 +0,0 @@
-import Container from "./container/Container";
-
-const components = {
-  Container,
-};
-
-export default components;
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,3 @@ export {
   LanguageProvider,
 } from "./components/context/LanguageContext";
 export { Theme, Media, BreakPoint } from "./constants/style";
-
-import Am from "./components/mobile";
-
-export { Am };
